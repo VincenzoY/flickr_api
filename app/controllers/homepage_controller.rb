18a9d75cf4571ea173fb314c53edeea900ac8c6f
Vincent Y <vincenzoyee@gmail.com>
2020-05-27 20:19:49 -0600

    Changed app to accept username instead of user id. Cleaned up the images so they display nicer.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,17 +4,14 @@ class HomepageController < ApplicationController
     end
 
     def create
-        @id ||= params["user"]["id"] unless params["user"] == nil
         @flickr = Flickr.new ENV['KEY'], ENV['SECRET']
         begin
+            @id = (@flickr.people.findByUsername :username => params["user"]["id"])["id"] 
             @photos = @flickr.people.getPublicPhotos :user_id => @id
         rescue
-            redirect_to :root, notice: "User not found"
+            flash.alert = "Not a valid User"
         end
+        render :index
     end
 
-    private
-
-    
-    
 end
